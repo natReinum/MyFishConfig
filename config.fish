64c827253787a718d09e81ec51dce7d926fc5c24
nmunier <nmunier@student.42.fr>
2026-01-29 18:11:29 +0100

    Initial commit

---
+++ config.fish
@@ -0,0 +1,48 @@
+function fish_greeting
+    echo '                            '(set_color 6B4B44)'@    '
+    echo '                           @     '
+    echo '                           @     '
+    echo '         '(set_color FFFF00)':'(set_color 6B4B44)'                @      '
+    echo '        '(set_color FFFF00)'+'(set_color 6B4B44)'                 @      '
+    echo '        '(set_color FFFF00)'+:'(set_color 6B4B44)'               @       '
+    echo '        '(set_color FFFF00)'++'(set_color 808080)'          @@@@@@       '
+    echo '        '(set_color FFFF00)'++'(set_color 808080)'            :@@@@@@@@@@'
+    echo '        '(set_color FFFF00)'+'(set_color FF5C00)'+'(set_color FFFF00)'+'(set_color blue)'           @'(set_color 6B4B44)'@@'(set_color blue)'@       '
+    echo '         '(set_color FFFF00)'+'(set_color FF5C00)'+'(set_color FFFF00)'+.'(set_color blue)'        @'(set_color 6B4B44)'@@@@'(set_color blue)'@      '
+    echo '         '(set_color FFFF00)'+'(set_color FF5C00)'++'(set_color FFFF00)'+'(set_color blue)'        @'(set_color 6B4B44)'@'(set_color 808080)'@'(set_color 6B4B44)'@'(set_color blue)'@       '
+    echo '        '(set_color FFFF00)'+'(set_color FF5C00)'++'(set_color FFFF00)'+        '(set_color yellow)'#'(set_color blue)'@'(set_color 6B4B44)'@'(set_color 808080)'@'(set_color 6B4B44)'@'(set_color blue)'@       '
+    echo '       '(set_color FFFF00)'+'(set_color FF5C00)'++'(set_color FFFF00)'+    '(set_color FFFF00)':    '(set_color blue)'@'(set_color 6B4B44)'@'(set_color 808080)'@'(set_color 6B4B44)'@'(set_color blue)'@'(set_color FF5C00)'.       '
+    echo '      '(set_color FFFF00)'+'(set_color FF5C00)'++'(set_color FFFF00)'+      +  '(set_color blue)'@'(set_color 6B4B44)'@'(set_color 808080)'@@'(set_color 6B4B44)'@'(set_color blue)'@        '
+    echo '     '(set_color FFFF00)'+'(set_color FF5C00)'++'(set_color FFFF00)'+  .     + '(set_color blue)'@'(set_color 6B4B44)'@'(set_color 808080)'@@'(set_color 6B4B44)'@'(set_color blue)'@        '
+    echo '    '(set_color FFFF00)'+'(set_color FF5C00)'++'(set_color FFFF00)'+   +     - '(set_color blue)'@'(set_color 6B4B44)'@'(set_color 808080)'@'(set_color 6B4B44)'@'(set_color blue)'@         '
+    echo '    '(set_color FFFF00)'+'(set_color FF5C00)'++'(set_color FFFF00)'+  '(set_color FF5C00)'+'(set_color FFFF00)'+    . '(set_color blue)'@'(set_color 6B4B44)'@'(set_color 808080)'@'(set_color 6B4B44)'@'(set_color blue)'@'(set_color yellow)'#         '
+    echo '    '(set_color FFFF00)'+'(set_color FF5C00)'++'(set_color FF0000)'++'(set_color FF5C00)'++'(set_color FFFF00)'=  '(set_color FF5C00)'-'(set_color FFFF00)'=  '(set_color blue)'@'(set_color 6B4B44)'@'(set_color 808080)'@'(set_color 6B4B44)'@'(set_color blue)'@          '
+    echo '      '(set_color FFFF00)'+'(set_color FF5C00)'+'(set_color FF0000)'++'(set_color FF5C00)'+'(set_color FFFF00)'+ '(set_color FF5C00)'+'(set_color FFFF00)'+  '(set_color blue)'@'(set_color 6B4B44)'@'(set_color 808080)'@@'(set_color 6B4B44)'@'(set_color blue)'@          '
+    echo '        '(set_color FFFF00)'+'(set_color FF5C00)'-'(set_color FF0000)'+++'(set_color FF5C00)'+'(set_color FFFF00)'+  '(set_color blue)'@'(set_color 6B4B44)'@'(set_color 808080)'@@'(set_color 6B4B44)'@'(set_color blue)'@          '
+    echo '        '(set_color FFFF00)'.'(set_color FF5C00)'+'(set_color FF0000)'-++='(set_color FF5C00)'+'(set_color FFFF00)'+'(set_color blue)'@'(set_color 6B4B44)'@'(set_color 808080)'@@'(set_color 6B4B44)'@'(set_color blue)'@           '
+    echo '         '(set_color FFFF00)'+-'(set_color FF0000)'++'(set_color FF5C00)'+ +'(set_color blue)'@'(set_color 6B4B44)'@'(set_color 808080)'@@'(set_color 6B4B44)'@'(set_color blue)'@           '
+    echo '         '(set_color FFFF00)'+'(set_color FF5C00)'-'(set_color FF0000)'++'(set_color FF5C00)'+  '(set_color blue)'@'(set_color 6B4B44)'@'(set_color 808080)'@'(set_color 6B4B44)'@'(set_color blue)'@'(set_color FFFF00)'.           '
+    echo '        '(set_color FFFF00)'+'(set_color FF5C00)'-'(set_color FF0000)'+ ++ '(set_color blue)'@'(set_color 6B4B44)'@'(set_color 808080)'@@'(set_color 6B4B44)'@'(set_color blue)'@'(set_color FF5C00)'+'(set_color FFFF00)'+          '
+    echo '       '(set_color FFFF00)'+'(set_color FF5C00)'+'(set_color FF0000)'-+ ++ '(set_color blue)'@'(set_color 6B4B44)'@'(set_color 808080)'@'(set_color 6B4B44)'@'(set_color blue)'@'(set_color yellow)'#'(set_color FF5C00)'++'(set_color FFFF00)'+         '
+    echo '      '(set_color FFFF00)'++'(set_color FF5C00)'-'(set_color FF0000)'-++-=%'(set_color blue)'@'(set_color 6B4B44)'@'(set_color 808080)'@'(set_color 6B4B44)'@'(set_color blue)'@'(set_color FF0000)'++'(set_color FF5C00)'='(set_color FFFF00)'++        '
+    echo '      '(set_color FFFF00)'.'(set_color FF5C00)'++'(set_color FFFF00)'-----'(set_color blue)'@'(set_color 6B4B44)'@'(set_color 808080)'@'(set_color 6B4B44)'@'(set_color blue)'@'(set_color FF0000)' .+'(set_color FF5C00)'=-'(set_color FFFF00)'++       '
+    echo '       '(set_color FFFF00)'+'(set_color FF5C00)'++'(set_color FF0000)'---#'(set_color blue)'@'(set_color 6B4B44)'@'(set_color 808080)'@'(set_color 6B4B44)'@'(set_color blue)'@ '(set_color FF0000)'.+'(set_color FF5C00)'--'(set_color FFFF00)'++       '
+    echo '        '(set_color FFFF00)'+'(set_color FF5C00)'+'(set_color FF0000)'+='(set_color 000000)'@@@@@@@'(set_color FF0000)'.#+'(set_color FF5C00)'--'(set_color FFFF00)'++       '
+    echo '         '(set_color FFFF00)'='(set_color yellow)'#'(set_color 000000)'@@@@@@@@@@@@'(set_color yellow)'+++       '
+    echo '      '(set_color FF5C00)'::'(set_color yellow)'-@+%'(set_color 000000)'@@@@@@@@@@'(set_color yellow)'***        '
+    echo '   '(set_color FF5C00)'*'(set_color FF0000)':#%'(set_color 000000)'@@@@@@#*#@****+++'(set_color FF0000)'=+'(set_color FF5C00)' **:    '
+    echo ' '(set_color FFFF00)'='(set_color FF5C00)'*'(set_color FF0000)'*+:-+'(set_color 000000)'***************'(set_color FF0000)'+=='(set_color FF5C00)'-*++*'(set_color FFFF00)'- '
+	echo (set_color normal)
+    echo Time: (set_color FF5C00)(date +%T)(set_color normal)
+    echo Hostname: (set_color FF5C00)$hostname
+	echo (set_color normal)
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -U fish_greeting
+
+alias cbc="xclip -selection clipboard"
+alias cbp="xclip -selection clipboard -o"
