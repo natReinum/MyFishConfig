a2e7cc5be6602f925a06fb33d6f7e4e2eccdb8a5
nmunier <nmunier@student.42.fr>
2026-02-13 13:39:51 +0100

    Add utility functions for managing iptables and updating fish config

--- config.fish
+++ config.fish
@@ -42,6 +42,24 @@ function make++
     find . -maxdepth 1 -type d ! -name . -exec make --no-print-directory -C {} $argv ';'
 end
 
+function alias update-fish-config
+	git clone https://github.com/natReinum/MyFishConfig.git /tmp/config_fish_temp
+	cat /tmp/config_fish_temp/config.fish > ~/.config/fish/config.fish
+    rm -fr /tmp/config_fish_temp
+end
+
+function block-ip
+    iptables -A INPUT -s $argv -j DROP
+end
+
+function open-port-tcp
+    iptables -A INPUT -p tcp --dport $argv -j ACCEPT
+end
+
+function open-port-udp
+    iptables -A INPUT -p udp --dport $argv -j ACCEPT
+end
+
 set -U fish_greeting
 
 alias cbc="xclip -selection clipboard"
